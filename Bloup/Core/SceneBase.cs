0758889078ec81376fadd4ef1926a16acc388972
PC PRO Antoine <antoine.zimmer.gambsheim@gmail.com>
2025-01-25 10:16:23 +0100

    HotFix base

--- Bloup/Core/SceneBase.cs
+++ Bloup/Core/SceneBase.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Bloup.Core;
 
-public abstract class SceneBase(SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
+public abstract class SceneBase(GraphicsDeviceManager graphics)
 {
     protected abstract string name { get; set; }
-    protected SpriteBatch _spriteBatch = spriteBatch;
-    protected GraphicsDeviceManager _graphics = graphics;
+    protected GraphicsDeviceManager Graphics = graphics;
 
     public string GetName()
     {
         return this.name;
     }
 
-    public abstract void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content);
+    public abstract void LoadContent(ContentManager content);
 
     public abstract void Update(GameTime gameTime);
 
-    public abstract void Draw(GameTime gameTime);
+    public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
 
 }
--- Bloup/Scenes/LevelScene.cs
+++ Bloup/Scenes/LevelScene.cs
@@ -5,11 +5,10 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Bloup.Scenes;
 
-public class LevelScene(SpriteBatch spriteBatch, GraphicsDeviceManager graphics) : SceneBase(spriteBatch, graphics)
+public class LevelScene(GraphicsDeviceManager graphics) : SceneBase(graphics)
 {
     protected override string name { get; set; } = "LevelScene";
 
@@ -17,7 +16,7 @@ public class LevelScene(SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
 
     private Texture2D background;
 
-    public override void Draw(GameTime gameTime)
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         spriteBatch.Begin();
         spriteBatch.Draw(background, new Vector2(0, 0), Color.Aqua);
