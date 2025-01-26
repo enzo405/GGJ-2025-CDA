68bfa23db160e1e3e9d13423cc01fc064d876f5c
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2025-01-26 17:27:27 +0100

    update color

--- Bloup/Scenes/GameOverScene.cs
+++ Bloup/Scenes/GameOverScene.cs
@@ -27,7 +27,7 @@ public class GameOverScene(ContentManager content, GraphicsDeviceManager graphic
     {
         spriteBatch.Begin();
         spriteBatch.DrawString(font, "Game Over", new Vector2(game?.ScreenHeight ?? 100, game?.ScreenWidth ?? 100), Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
-        spriteBatch.DrawString(font, "Press any key to restart", new Vector2(game?.ScreenHeight ?? 200, game?.ScreenWidth ?? 200), Color.Red, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+        spriteBatch.DrawString(font, "Press any key to restart", new Vector2(game?.ScreenHeight ?? 200, game?.ScreenWidth ?? 200), Color.Purple, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
 
         spriteBatch.End();
     }
