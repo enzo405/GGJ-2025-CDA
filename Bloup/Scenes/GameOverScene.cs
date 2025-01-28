8542a6bf19eae024c1df3ed3d27751feb450b1bc
enzo-chaboisseau <enzo.chaboisseau@gmail.com>
2025-01-28 16:27:28 +0100

    a

--- Bloup/Scenes/GameOverScene.cs
+++ Bloup/Scenes/GameOverScene.cs
@@ -27,14 +27,14 @@ public class GameOverScene(ContentManager content, GraphicsDeviceManager graphic
     {
         spriteBatch.Begin();
         spriteBatch.DrawString(font, "Game Over", new Vector2(game?.ScreenHeight ?? 100, game?.ScreenWidth ?? 100), Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
-        spriteBatch.DrawString(font, "Press any key to restart", new Vector2(game?.ScreenHeight ?? 200, game?.ScreenWidth ?? 200), Color.Purple, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+        spriteBatch.DrawString(font, "Press enter to restart", new Vector2(game?.ScreenHeight ?? 200, game?.ScreenWidth ?? 200), Color.White, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
 
         spriteBatch.End();
     }
 
     public override void Update(GameTime gameTime)
     {
-        if (Keyboard.GetState().GetPressedKeys().Length > 0)
+        if (Keyboard.GetState().IsKeyDown(Keys.Enter))
         {
             SceneManager.Create(game).ChangeScene("LevelScene");
         }
